baa4d6262a01ade4b7b568bb6bad1434de227b0b
Enid Okemwa <okemwaenid23@gmail.c>
2023-10-16 22:38:25 -0700

    Simple shell task 0

---
+++ Betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+printf("size of a char : % ld bytes \n", sizeof(char));
+printf("size of a int : % ld bytes \n", sizeof(int));
+printf("size of a float : % ld bytes \n", sizeof(float));
+return (0);
+}
